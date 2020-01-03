3e6d0b365ac3616cb24c5931238e93ee10cb2536
Joseph Jennings <josephjennings@Josephs-MacBook-Pro.local>
2020-01-03 11:13:24 -0800

    Set up foundation of CatTinder Frontend

---
+++ src/pages/Cats.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Cats extends Component {
+  render() {
+    return (
+        <div>
+            <h1>Cats</h1>
+        </div>
+    );
+  }
+}
+
+export default Cats;
---
+++ src/pages/NewCat.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewCats extends Component {
+  render() {
+    return (
+        <div>
+            <h1>New Cats</h1>
+        </div>
+    );
+  }
+}
+
+export default NewCats;
